facdcfc7fe912fa78f7b52396fc9af48cfb7bf53
2011shao <562797023@qq.com>
2024-01-08 14:02:34 +0800

    tjiao

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/superClass',
+  base: './',
   server: {
     host: true
   },
