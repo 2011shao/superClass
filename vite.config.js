416f8f9e1c5e4504aa92f23cc6b53ba2697443c3
2011shao <562797023@qq.com>
2024-01-22 18:14:15 +0800

    部署

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   server: {
     host: true
   },
